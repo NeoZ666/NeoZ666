19efb403558dd1124a62dca3149583e1611f324b
Neo <115883668+NeoZ666@users.noreply.github.com>
2025-04-06 23:22:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
 const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
-const progressBarOfThisYear = generateProgressBar()
+const progressBarOfThisYear = generateProgressBar(progressOfThisYear)
 
-function generateProgressBar() {
+function generateProgressBar(progress) {
     const progressBarCapacity = 30
-    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const passedProgressBarIndex = parseInt(progress * progressBarCapacity)
     const progressBar =
       '█'.repeat(passedProgressBarIndex) +
       '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
